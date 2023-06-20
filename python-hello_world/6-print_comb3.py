2c7965fc73cb76e22fb0ce5a5e901a12bfef70c3
root <root@localhost.localdomain>
2023-06-19 23:55:12 -0400

    5.1.2

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(10):
     for y in range(x + 1, 10):
         if x != y:
-            print("{:02d}, {:02d}, ".format(x, y), end="")
+            print("{:d}{:d}, ".format(x, y), end="")
 
 print("89")
