6a2980171ad1aff872533b45775f6ad6fb9879b7
root <root@localhost.localdomain>
2023-06-19 23:47:07 -0400

    5.1.1

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,7 @@
-for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        if num1 < 8:
-            print("{:d}{:d}, ".format(num1, num2), end="")
-        else:
-            print("{:d}{:d}".format(num1, num2))
+#!/usr/bin/python3
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x != y:
+            print("{:02d}, {:02d}, ".format(x, y), end="")
+
+print("89")
